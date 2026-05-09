0a525952d6727d49078497685ed99d64b729e157
Abdelrahman <86893144+AbdelrahmanOmar1@users.noreply.github.com>
2026-05-09 17:12:58 +0300

    Update axios baseURL configuration
    Removed trailing slash from baseURL in axios configuration.

--- govTime-frontend/src/axiosConfig.js
+++ govTime-frontend/src/axiosConfig.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "https://data-process-analytics-api-production.up.railway.app/",
+  baseURL: "https://data-process-analytics-api-production.up.railway.app",
   withCredentials: true,
   headers: {
     "Content-Type": "application/json",
