c3eaea416a02364f979fbd502ec706597a465c51
Abdelrahman <86893144+AbdelrahmanOmar1@users.noreply.github.com>
2026-05-09 17:15:54 +0300

    Update axiosConfig.js

--- govTime-frontend/src/axiosConfig.js
+++ govTime-frontend/src/axiosConfig.js
@@ -3,9 +3,6 @@ import axios from "axios";
 const api = axios.create({
   baseURL: "https://data-process-analytics-api-production.up.railway.app",
   withCredentials: true,
-  headers: {
-    "Content-Type": "application/json",
-  },
 });
 
 export default api;
