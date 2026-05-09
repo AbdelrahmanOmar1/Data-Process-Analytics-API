a0c59e54488de45aaa0d17b66c55f5c0b08fa9d2
Abdelrahman <86893144+AbdelrahmanOmar1@users.noreply.github.com>
2026-05-09 16:11:08 +0300

    Delete routes directory

--- routes/anlayticsRoutes.js
+++
@@ -1,9 +0,0 @@
-const router = require("express").Router();
-const controller = require("../controllers/analyticsController");
-
-router.get("/top-items", controller.topItems);
-router.get("/deadstock", controller.deadStock);
-router.get("/profit", controller.profitReport);
-router.get("/search", controller.searchItem);
-
-module.exports = router;
--- routes/uploadRoutes.js
+++
@@ -1,8 +0,0 @@
-const router = require("express").Router();
-const upload = require("../middleware/upload");
-const controller = require("../controllers/uploadController");
-
-router.post("/sales", upload.single("file"), controller.uploadSales);
-router.post("/purchase", upload.single("file"), controller.uploadPurchase);
-
-module.exports = router;
